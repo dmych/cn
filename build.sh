d43477cc4440fe10140e9841ac9af62a28215f7d
dmych <dbrechalov@gmail.com>
2011-10-21 00:26:06 +0400

    Building deb-package

--- build.sh
+++ build.sh
@@ -8,8 +8,14 @@ echo "Building package $PKG_NAME"
 mkdir -p build/opt/cn
 mkdir -p build/DEBIAN
 mkdir -p build/usr/local/bin
+mkdir -p build/usr/share/applications
+mkdir -p build/usr/share/pixmaps
 cp -v README.markdown icon.png *.py *.ui build/opt/cn
 cp -v control build/DEBIAN
+cp -v postinst build/DEBIAN
+cp -v postrm build/DEBIAN
+cp -v cn.desktop build/usr/share/applications
+cp -v icon.png build/usr/share/pixmaps/cn.png
 cd build/usr/local/bin
 ln -sv ../../../opt/cn/cn.py cn
 cd $PROG_DIR
